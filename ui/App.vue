5a68d1a3dc421ca13cdd43fdc8bcaba3e7374451
Ste Riley <ste@steriley.com>
2025-07-07 21:46:56 +0100

    feat: tanstack vue form quickstart

--- ui/App.vue
+++ ui/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 
+import FindersForm from './components/FindersForm.vue';
+
 const ducks = ref('');
 
 onMounted(async () => {
@@ -13,19 +15,5 @@ onMounted(async () => {
 
 <template>
   <h2>Hello {{ ducks }}</h2>
+  <FindersForm />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
